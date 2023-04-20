f328067188f7ce45433b225072204b25eba121cf
Thinh <thinhvt45@gmail.com>
2023-04-20 22:57:07 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const video = document.getElementById('video1');
-const canvas1 = document.getElementById('canvas1');
-const canvas2 = document.getElementById('canvas2');
-const ctx1 = canvas1.getContext('2d');
-const ctx2 = canvas2.getContext('2d');
+// Lấy các phần tử HTML cần thiết
+var video = document.getElementById('video');
+var canvas1 = document.getElementById('canvas1');
+var canvas2 = document.getElementById('canvas2');
+var ctx1 = canvas1.getContext('2d');
+var ctx2 = canvas2.getContext('2d');
 
-video.addEventListener('play', function() {
-  var $this = this; // caching
-  (function loop() {
-    if (!$this.paused && !$this.ended) {
-      ctx1.drawImage($this, 0, 0, canvas1.width, canvas1.height);
-      var imgData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
-      var imgDataCopy = new ImageData(imgData.width, imgData.height);
-      for (let i = 0; i < imgData.data.length; i += 4) {
-        imgDataCopy.data[i] = imgData.data[i + 1];
-        imgDataCopy.data[i + 1] = imgData.data[i + 2];
-        imgDataCopy.data[i + 2] = imgData.data[i + 3];
-        imgDataCopy.data[i + 3] = imgData.data[i];
-      }
-      ctx2.putImageData(imgDataCopy, 0, 0);
-      setTimeout(loop, 0); // drawing at 60fps
-    }
-  })();
-});
+// Thiết lập kích thước của canvas bằng với kích thước của video
+canvas1.width = video.width;
+canvas1.height = video.height;
+canvas2.width = video.width;
+canvas2.height = video.height;
+
+// Bắt đầu chạy video
+video.play();
+
+// Xử lý hình ảnh từ video
+function processVideo() {
+	// Vẽ hình ảnh từ video lên canvas1
+	ctx1.drawImage(video, 0, 0, canvas1.width, canvas1.height);
+
+	// Lấy dữ liệu hình ảnh từ canvas1
+	var imageData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
+	var data = imageData.data;
+
+	// Xử lý dữ liệu hình ảnh
+	// Trong ví dụ này, chúng ta sẽ đổi màu các pixel thành màu đỏ
+	for (var i = 0; i < data.length; i += 4) {
+		data[i] = 255;
+		data[i+1] = 0;
+		data[i+2] = 0;
+	}
+
+	// Đưa dữ liệu hình ảnh đã xử lý lên canvas2
+	ctx2.putImageData(imageData, 0, 0);
+
+	// Lặp lại xử lý cho frame tiếp theo
+	requestAnimationFrame(processVideo);
+}
+
+// Bắt đầu xử lý video và canvas
+processVideo();
